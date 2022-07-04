f7cede12a504a59fc0f1e72eb832c25ab11b931f
Nadison Fidely <fidelysnad@exemple.com>
2022-07-04 15:43:33 +0200

    début du projet

---
+++ pomodoro/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+
+function App() {
+  return (
+   <main>
+       <h1>Pomodoro Timer</h1>
+   </main>  );
+}
+
+export default App;
